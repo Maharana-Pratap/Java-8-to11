906d39b9c8a3ad43a8a82af7b0a7f86c7fd71664
Maharana <maharana1982@gmail.com>
2022-12-20 16:41:56 +0530

    Grouping by updated

--- src/test/GroupBy.java
+++ src/test/GroupBy.java
@@ -10,6 +10,7 @@ public class GroupBy {
 	public static void main(String[] args) {
 		List<String> list = List.of("java",".net","php","java");
 		
+		// Find frequecny of input value
 		Map<String,Long> result = list.stream()
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 		

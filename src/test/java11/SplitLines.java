ca4cc0b2c654d3a5ca11256800f75e4ff17f3e36
Maharana <maharana1982@gmail.com>
2023-03-28 14:01:25 +0530

    code updated

--- src/test/java11/SplitLines.java
+++ src/test/java11/SplitLines.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SplitLines {
-
 	public static void main(String[] args) {
 		String linesString = "Maharana\nPratap\nSingh";
 		System.out.println(linesString);

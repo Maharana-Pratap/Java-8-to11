7795fe0b2aae96ea766e56cdaf867b9a94604394
Maharana <maharana1982@gmail.com>
2023-03-28 14:09:06 +0530

    isBlank code updated

--- src/test/java11/IsBlankString.java
+++ src/test/java11/IsBlankString.java
@@ -1,9 +1,8 @@
 package test.java11;
 
 public class IsBlankString {
-
 	public static void main(String[] args) {
-		System.out.println(" ".isBlank());
-		System.out.println(" ".isEmpty());
+		System.out.println(" ".isBlank());  // true
+		System.out.println(" ".isEmpty()); // false
 	}
 }
